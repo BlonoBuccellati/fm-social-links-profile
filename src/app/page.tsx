b6df200db32df9c3a76afb052b8c70ac6957a597
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-03-09 21:23:27 +0900

    style: apply basic styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 export default function Home() {
-  const buttons = [
+  const applicationButtons = [
     { name: 'GitHub', url: '#' },
     { name: 'Frontend Mentor', url: '#' },
     { name: 'LinkedIn', url: '#' },
@@ -8,28 +8,39 @@ export default function Home() {
     { name: 'Instagram', url: '#' },
   ];
   return (
-    <div className='bg-gray-900 min-h-screen text-white'>
+    <div className='bg-gray-900 text-white flex justify-center items-center min-h-screen'>
       {/* TODO:本来、複数ユーザがいるはずなので、ユーザ情報を引数にして、カードをコンポーネント化する。 */}
       {/* Status Card */}
-      <div>
+      <div className='bg-gray-800 flex flex-col  items-center w-100 h-150 rounded-2xl'>
         {/* Content */}
-        <Image
-          src={'/assets/images/avatar-jessica.jpeg'}
-          alt='user icon'
-          width={100}
-          height={100}
-        />
-
-        <div>Jessica Randall</div>
-        <div>London, United Kingdom</div>
-        <p>&ldquo;Front-end developer and avid reader.&rdquo;</p>
+        <div className='flex flex-col justify-center items-center mt-10'>
+          <Image
+            src={'/assets/images/avatar-jessica.jpeg'}
+            alt='user icon'
+            width={100}
+            height={100}
+            className='rounded-full'
+          />
+          <div className='text-2xl font-bold'>Jessica Randall</div>
+          <div className='text-lime-300 font-bold text-[14px]'>
+            London, United Kingdom
+          </div>
+          <p>&ldquo;Front-end developer and avid reader.&rdquo;</p>
+        </div>
         {/* TODO:ボタンはコンポーネントにする。 */}
         {/* Buttons */}
-        {buttons.map((button, index) => (
-          <button key={index}>
-            <a href={button.url}>{button.name}</a>
-          </button>
-        ))}
+        <div className='flex flex-col gap-3.5'>
+          {applicationButtons.map((button, index) => (
+            <button
+              key={index}
+              className='bg-gray-700 w-80 h-10 rounded-lg hover:cursor-pointer hover:bg-lime-300 hover:text-gray-700'
+            >
+              <a href={button.url} className='font-bold'>
+                {button.name}
+              </a>
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
