de8f24a7791b2a4a68cc13a966d995004e2b5c30
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-03-16 13:27:51 +0900

    refactor: 不要なサイズ指定を削除し、に変更。カードに対してサイズ指定を追加

--- src/app/_components/buttons.tsx
+++ src/app/_components/buttons.tsx
@@ -8,11 +8,11 @@ const links = [
 
 const ApplicationButtons = () => {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex w-full flex-col gap-4">
       {links.map((button, index) => (
         <button
           key={index}
-          className="h-[45px] w-[279px] rounded-lg bg-gray-700 hover:cursor-pointer hover:bg-lime-300 hover:text-gray-700 md:w-76"
+          className="h-[45px] w-full rounded-lg bg-gray-700 hover:cursor-pointer hover:bg-lime-300 hover:text-gray-700"
         >
           <a href={button.url} className="text-sm font-bold">
             {button.name}
